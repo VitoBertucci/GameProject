61e6066854d5b485b78e054f61d21378a30a5e13
VitoBertucci <vito.rob.bertucci@gmail.com>
2022-11-17 16:19:59 -0500

    project

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -17,10 +17,12 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
+
         Parent root = FXMLLoader.load(getClass().getResource("primary.fxml"));
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+        
     }
 
     public static void main(String[] args) {
--- demo/src/main/java/com/example/PrimaryController.java
+++ demo/src/main/java/com/example/PrimaryController.java
@@ -1,19 +1,25 @@
 package com.example;
 import java.io.IOException;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 
 public class PrimaryController {
     @FXML
     private Label label;
     @FXML
     private Pane mapPane;
+    @FXML
+    private VBox box;
+    
+    
+
     
     
     @FXML
     private void buttonPress() throws IOException {
         mapPane.getChildren().add(new TileMap(15));
-        
     }
 }
